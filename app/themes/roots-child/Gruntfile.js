75d98c15082f0d6a25e489ad1c4bc3d4d904b6d4
Michael Bianco <iloveitaly@gmail.com>
2014-11-29 13:06:34 -0500

    Adding basic roots-child

---
+++ app/themes/roots-child/Gruntfile.js
@@ -0,0 +1,133 @@
+'use strict';
+module.exports = function(grunt) {
+  // Load all tasks
+  require('load-grunt-tasks')(grunt);
+  // Show elapsed time
+  require('time-grunt')(grunt);
+
+  var jsFileList = [
+
+  ];
+
+  grunt.initConfig({
+    less: {
+      dev: {
+        files: {
+          'style.css': [
+            'style.less'
+          ]
+        },
+        options: {
+          compress: false,
+          // LESS source map
+          // To enable, set sourceMap to true and update sourceMapRootpath based on your install
+          sourceMap: true,
+          sourceMapFilename: 'style.css.map',
+          sourceMapRootpath: ''
+        }
+      },
+      build: {
+        files: {
+          'style.min.css': [
+            'style.less'
+          ]
+        },
+        options: {
+          compress: true
+        }
+      }
+    },
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: [jsFileList],
+        dest: 'scripts.js',
+      },
+    },
+    uglify: {
+      dist: {
+        files: {
+          'scripts.min.js': [jsFileList]
+        }
+      }
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions', 'ie 8', 'ie 9', 'android 2.3', 'android 4', 'opera 12']
+      },
+      dev: {
+        options: {
+          map: {
+            prev: 'assets/css/'
+          }
+        },
+        src: 'style.css'
+      },
+      build: {
+        src: 'style.min.css'
+      }
+    },
+    version: {
+      default: {
+        options: {
+          format: true,
+          length: 32,
+          manifest: 'assets/manifest.json',
+          querystring: {
+            style: 'roots_css',
+            script: 'roots_js'
+          }
+        },
+        files: {
+          'lib/scripts.php': 'assets/{css,js}/{main,scripts}.min.{css,js}'
+        }
+      }
+    },
+    watch: {
+      less: {
+        files: [
+          'assets/less/*.less',
+          'assets/less/**/*.less'
+        ],
+        tasks: ['less:dev', 'autoprefixer:dev']
+      },
+      js: {
+        files: [
+          jsFileList,
+        ],
+        tasks: ['concat']
+      },
+      livereload: {
+        // Browser live reloading
+        // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
+        options: {
+          livereload: false
+        },
+        files: [
+          'style.css',
+          // 'assets/js/scripts.js',
+          'templates/*.php',
+          '*.php'
+        ]
+      }
+    }
+  });
+
+  // Register tasks
+  grunt.registerTask('default', [
+    'dev'
+  ]);
+  grunt.registerTask('dev', [
+    'less:dev',
+    'autoprefixer:dev',
+    'concat'
+  ]);
+  grunt.registerTask('build', [
+    'less:build',
+    'autoprefixer:build',
+    'uglify',
+    'version'
+  ]);
+};
